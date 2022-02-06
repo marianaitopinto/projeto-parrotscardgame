23b9a1ccd452a10d537a8d8b0873aeb34c2f38f4
Mariana Ito <marianaitopinto@gmail.com>
2022-02-06 13:07:06 -0300

    Inclusão do contador, checagem de igualdade, fim do jogo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 let numeroCartas = parseInt(prompt("Com quantas cartas deseja jogar? Números pares de 4 a 14 :D"));
 cartasGif = [];
-let nomeCartas = ["par1", "par1", "par2", "par2", "par3", "par3", "par4", "par4", "par5", "par5", "par6", "par6", "par7", "par7"]
 let cartasAbertas = [];
+let cartaUm = null;
+let CartaDois = null;
+let armazenarPares = 0;
+let qtdJogadas = 0;
+let contador = document.querySelector(".numJogadas");
 
 function qtdCartas() {
     while ((numeroCartas % 2 !== 0) || (numeroCartas < 4) || (numeroCartas > 14) || (numeroCartas === 0)) {
@@ -17,15 +21,20 @@ function comparador() {
 }
 
 function adicionarCartas() {
-    for (i = 0; i < numeroCartas; i++) {
-        cartasGif.push(`arquivos/${i}.gif`);
-        cartasGif.sort(comparador);
+    for (i = 0; i < numeroCartas/2; i++) {
+        cartasGif.push(`arquivos/${i}.gif`);       
+    }
+    for (i = 0; i < numeroCartas/2; i++) {
+        cartasGif.push(`arquivos/${i}.gif`);        
     }
 
+    cartasGif.sort(comparador);
+
     for (let x = 0; x < numeroCartas; x++) {
+        
         const incluir = document.querySelector("main");
         incluir.innerHTML = incluir.innerHTML + `
-    <div class="cartas" onclick="selecionarCarta(this,'${nomeCartas[x]}')">
+    <div class="cartas selecionada" onclick="selecionarCarta(this,'${cartasGif[x]}')">
         
         <div class="verso face">
             <img src="${cartasGif[x]}" alt="GifsAleatorios">
@@ -40,26 +49,43 @@ function selecionarCarta(opcao, nomeExibicao) {
 
     if (cartasAbertas.length < 2) {
         if (selecionado.classList.contains("verso")) {
-
             selecionado.classList.remove("verso");
             cartasAbertas.push(nomeExibicao);
-
-            
+            qtdJogadas++;
+            contador.innerHTML = `Jogadas: ${qtdJogadas}`;
         }
     }
-    if (cartasAbertas == 2) {
-        checarIgualdade(opcao);
+    if (cartasAbertas.length == 2) {
+        checarIgualdade();
     }
 }
 
 function checarIgualdade() {
-    if (cartasAbertas[0] !== cartasAbertas[1]) {
-        setTimeout(fecharCartas, 1000);
+    cartaUm = cartasAbertas[0];
+    cartaDois = cartasAbertas[1];
+
+    if (cartaUm !== cartaDois) {
+        fecharCartas();
     } 
+
+    else {        
+        armazenarPares = armazenarPares + 2;
+        if (armazenarPares < numeroCartas) {
+            cartasAbertas = [];        
+        } else if (armazenarPares == numeroCartas) {
+            setTimeout(alertar, 1000);
+        }
+    }
     
 }
 
 function fecharCartas() {
-    cartasAbertas[0].classList.add ("verso");
-    cartasAbertas[1].classList.add ("verso");
+    let fechar = document.querySelectorAll(".adicionado");
+    fechar.classList.add("verso");
+    //cartaUm.classList.add ("verso");
+    //cartaDois.classList.add ("verso");
+}
+
+function alertar() {
+    alert(`Fim do jogo! Você ganhou em ${qtdJogadas} jogadas.`);
 }
